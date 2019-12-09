1998b6f377642cd5495c1c31b84f4d08d5367a1f
jakakokosar <jaka.kokosar@gmail.com>
2019-12-09 12:58:26 +0100

    refactor project structure

--- image_comments/main.py
+++ image_comments/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
+from starlette_prometheus import metrics, PrometheusMiddleware
+
 from .routers import comments
+from .utils import check_liveness, check_readiness
+
+VERSION = '1.0.0'
+API_VERSION = 'v1'
+PREFIX = f'/api/{API_VERSION}/comments'
+
 
 app = FastAPI(
     title='image-comments',
     description='Microservice for handling image comments',
-    version='1.0.0',
-    openapi_url='/api/v1/openapi.json')
+    version=VERSION,
+    openapi_url=f'{PREFIX}/openapi.json',
+)
 
+
+app.add_middleware(PrometheusMiddleware)
+app.add_route(f'{PREFIX}/metrics/', metrics)
 
 app.include_router(
-    comments.router,
-    prefix='/api/v1',
-    responses={404: {'description': 'Not found'}},
+    comments.router, prefix=PREFIX, responses={404: {'description': 'Not found'}},
 )
+
+app.add_route(f'{PREFIX}/health/live', check_liveness)
+app.add_route(f'{PREFIX}/health/ready', check_readiness)
--- image_comments/routers/comments.py
+++ image_comments/routers/comments.py
@@ -1,22 +1,50 @@
 import uuid
 
+# import logging
 
 from fastapi import APIRouter
 from image_comments import settings
 
+# import json_log_formatter
+
+#
+# formatter = json_log_formatter.JSONFormatter()
+# json_handler = logging.StreamHandler()
+# json_handler.setFormatter(formatter)
+# logger = logging.getLogger('uvicorn')
+# logger.addHandler(json_handler)
+# logger.setLevel(logging.INFO)
+
+
 router = APIRouter()
 
 
-@router.get("/")
+@router.get('/settings')
 async def test_configs():
-    return {"Config for X:": f"{settings.config_x}",
-            "Config for Y:": f"{settings.config_y}"}
+    return {"Config for X:": f"{settings.config_x}", "Config for Y:": f"{settings.config_y}"}
 
 
 def comment_generator():
     return [{'id': index, 'author_id': uuid.uuid1(), 'text': 'Test comment ' + str(index)} for index in range(1, 6)]
 
 
-@router.get("/comments/")
-async def list_comments():
+# ordering of decorators is very important
+@router.get('/')
+def list_comments():
+    # logger.info('function1 has executed',
+    #         extra={
+    #             'job_category': 'test_function',
+    #             'logger.name': 'my_json',
+    #         }
+    #     )
+    #     logger = logging.getLogger("uvicorn")
+    #     print(logger.__dict__)
+    #     print([name for name in logging.root.manager.loggerDict])
+    # import time
+    #
+    # time.sleep(2)
+    #
+    # logger = logging.getLogger("router")
+    # logger.info("THIS IS A INFO LOG", extra={'a': 1})
+
     return comment_generator()
